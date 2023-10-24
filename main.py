361d02171e04ed6ed1a9a268c0757f268664a55e
Jacob Frost <jacobfrost@jacobs-mbp.lan>
2023-10-24 18:19:22 -0400

    List names of all resources in tree view cd /Users/jacobfrost/Documents/Python/BuildermentCalculatorPython ; /usr/bin/env /Users/jacobfrost/Documents/Python/BuildermentCalculatorPython/venv/bin/python /Users/jacobfrost/.vscode/extensions/ms-python.python-2023.18.0/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 51334 -- /Users/jacobfrost/Documents/Python/BuildermentCalculatorPython/main.py

--- main.py
+++ main.py
@@ -27,3 +27,25 @@ wolframiteExtractor = 10
 
 # We also want belt speed
 beltItemsPerMinute = 420
+
+def calculateResourceUsageForRecipe(recipeName, depth = 0):
+    tabs = "  " * depth
+    print(tabs, end="")
+    print(f"{recipeName}")
+
+    # Define our base case (prevent infinite loop)
+    if recipeName in rawResources:
+        return
+
+    for recipe in recipes:
+        if recipe["name"] == recipeName:
+            for subRecipe in recipe["ingredientList"]:
+                calculateResourceUsageForRecipe(subRecipe["name"], depth + 1)
+
+# Actually start the script
+resourceToMax = "Electromagnet" #input("Name of resource you would like to maximize: ")
+
+for recipe in recipes:
+    if recipe["name"] == resourceToMax:
+
+        calculateResourceUsageForRecipe(recipe["name"])
