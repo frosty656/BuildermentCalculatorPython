1bae1f588cbb4bd14e112524bc94ca64b4ee363a
Jacob Frost <jacobfrost@jacobs-mbp.lan>
2023-10-24 18:20:54 -0400

    Created helped functions

--- main.py
+++ main.py
@@ -28,6 +28,60 @@ wolframiteExtractor = 10
 # We also want belt speed
 beltItemsPerMinute = 420
 
+def level_multiplier(level):
+    if level == 1:
+        return 1
+    elif level == 2:
+        return 1.5
+    elif level == 3:
+        return 2
+    elif level == 4:
+        return 3
+    elif level == 5:
+        return 4
+    else:
+        return 1
+    
+def get_building_level(name):
+    name_lower = name.lower()
+    
+    if name_lower == "workshop":
+        return workshopLevel
+    elif name_lower == "furnace":
+        return furnaceLevel
+    elif name_lower == "machine shop":
+        return machineShopLevel
+    elif name_lower == "industrial factory":
+        return industrialFactoryLevel
+    elif name_lower == "forge":
+        return forgeLevel
+    elif name_lower == "manufacturer":
+        return manufacturerLevel
+    elif name_lower == "extractor":
+        return extractorLevel
+    elif name_lower == "earth transporter":
+        return 1
+    else:
+        return 1
+
+def get_extractor_count_by_resource(variable_name):
+    variable_name_lower = variable_name.lower()
+
+    if variable_name_lower == "copper ore":
+        return copperOreExtractor
+    elif variable_name_lower == "iron ore":
+        return ironOreExtractor
+    elif variable_name_lower == "wood log":
+        return woodLogExtractor
+    elif variable_name_lower == "stone":
+        return stoneExtractor
+    elif variable_name_lower == "coal":
+        return coalExtractor
+    elif variable_name_lower == "wolframite":
+        return wolframiteExtractor
+    else:
+        return 1
+
 def calculateResourceUsageForRecipe(recipeName, depth = 0):
     tabs = "  " * depth
     print(tabs, end="")
